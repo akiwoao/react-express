de60b01b55eaa3169409900251e3d94216632aa5
TetsuyaAoki <zheyeqingmu5@gmail.com>
2022-11-07 16:26:59 +0900

    ページ遷移実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useState, useEffect } from "react";
+// react-router-domのインポートを追加
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+
+//ページの追加
+import { Home } from "./pages/Home";
+import { Stockinfo } from "./pages/Stockinfo";
+import { Stockpredict } from "./pages/Stockpredict";
 
 function App() {
-    const [message, setMessage] = useState("");
-    useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => setMessage(data.message));
-    }, []);
     return (
-        <div className="App">
-            <h1>フロントエンド</h1>
-            <p>{message}</p>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <NavLink to="/">Home</NavLink>
+                <br />
+                <NavLink to="/stockinfo">Stockinfo</NavLink>
+                <br />
+                <NavLink to="/stockpredict">Stockpredict</NavLink>
+                <br />
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="stockinfo" element={<Stockinfo />} />
+                    <Route path="stockpredict" element={<Stockpredict />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
---
+++ frontend/src/pages/Stockinfo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Stockinfo = () => {
+    return (
+        <div>
+            <h1>Stockinfo</h1>
+        </div>
+    );
+};
